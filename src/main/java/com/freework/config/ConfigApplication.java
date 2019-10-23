e7590371f1ac83c8038ad46b3944e0e36cb44e41
daihongru <daihr98@foxmail.com>
2019-10-24 00:01:20 +0800

    去除bus功能

--- src/main/java/com/freework/config/ConfigApplication.java
+++ src/main/java/com/freework/config/ConfigApplication.java
@@ -2,14 +2,14 @@ package com.freework.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author daihongru
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableConfigServer
 public class ConfigApplication {
 
